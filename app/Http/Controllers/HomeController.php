bfa448ba544bca5231f81074097c0b53d5a943d8
lenovo-PC\lenovo <sanaullah@pakipreneurs.com>
2017-01-12 08:48:55 +0500

    added post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = \DB::table('posts')->get();
+        return view('home',["posts"=>$posts]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,59 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <!-- Table -->
+            <table class="table table-striped table-bordered table-hover dt-responsive" width="100%" id="genral">
+
+                <!-- Table heading -->
+                <thead class="bg-gray">
+                <tr>
+                    <th>Post Name</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <!-- // Table heading END -->
+
+                <!-- Table body -->
+                <tbody>
+
+
+                <?php
+                foreach($posts as $post){
+                //print_r($post);exit;
+                ?>
+                <!-- Table row -->
+                <tr class="gradeX">
+                    <td   ><?php echo $post->post_title;?></td>
+                    <td><?php echo $post->post_content?></td>
+                    <td>
+                        <a  href="#" onclick="callCrudAction('users',<?php echo $post->id;?>,'delete_data')" id="<?php echo $post->id;?>">
+                            <span class="glyphicon glyphicon-remove"></span>
+                        </a>
+                        <a  href="<?php  url();?><?php echo $post->id;?>/edit_post" >
+                            <span class="glyphicon glyphicon-pencil"></span>
+                        </a>
+                    <!--<a  href="<?php  url();?>alpha/reset_password/<?php echo $post->id;?>" >
+                          <span class="glyphicon glyphicon-home"></span>
+                      </a>-->
+                    </td>
+
+                </tr>
+                <!-- // Table row END -->
+
+                <?php } ?>
+
+
+
+
+                </tbody>
+                <!-- // Table body END -->
+
+            </table>
+            <!-- // Table END -->
+        </div>
+    </div>
 </div>
 @endsection
