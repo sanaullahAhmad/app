59de643c42ae2d9906f45dde950a20d3aa258140
lenovo-PC\lenovo <sanaullah@pakipreneurs.com>
2017-01-12 09:13:15 +0500

    adding auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        Post::class => PostPolicy::class,
     ];
 
     /**
@@ -24,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Gate::define('update-post', function ($user, $post) {
+            return $user->id == $post->user_id;
+        });
 
         //
     }
